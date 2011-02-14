6e1758b48a6d65ee419082eae5b420b5d567e298
juraj_slk <juraj_slk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-14 19:50:46 +0000

    Typo3 4.5 compatible
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_filelinks/trunk@43583 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -14,6 +14,7 @@ if ($tempSetup['ctype_media_add_orig_field']) {
 	t3lib_extMgm::addToAllTCAtypes('tt_content','tx_damfilelinks_filelinks','uploads','after:select_key');
 } else {
 	$TCA['tt_content']['types']['uploads']['showitem'] = str_replace(', media;', ', tx_damfilelinks_filelinks;', $TCA['tt_content']['types']['uploads']['showitem']);
+	$TCA['tt_content']['palettes']['uploads']['showitem'] = str_replace(', media;', ', tx_damfilelinks_filelinks;', $TCA['tt_content']['palettes']['uploads']['showitem']);
 }
 
 $TCA['tt_content']['columns']['tx_damfilelinks_filelinks']['config']['allowed_types'] = $tempSetup['allowedExt'];
